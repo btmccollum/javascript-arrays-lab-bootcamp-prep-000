7393d0ced1c41c10471037ec467fded33ccf6088
Brad McCollum <btmcco8@gmail.com>
2018-05-13 22:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   return kittens.pop();
 }
+
+function destrutivelyRemoveFirstKitten(){
+  return kittens.push();
+}
